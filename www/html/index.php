b0a88afff338a68e9ff056599ed52713af5e4d65
Manotiwak <129669781+Manotiwak@users.noreply.github.com>
2023-06-05 23:29:44 +0200

    Add files via upload

---
+++ www/html/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>  
+     <head>  
+          <title>lab11</title>
+     </head>  
+
+     <body>  
+          <h1>Mateusz Nowak</h1>
+          <p><?php echo 'PHP, version: ' . phpversion(); ?></p>
+     </body>
